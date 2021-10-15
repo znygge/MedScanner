d275e1abdfa5aa0d91cd994087ba71764ba89082
Patrik Johansson <patrikjohansson@Patriks-MacBook-Pro.local>
2021-10-15 18:27:39 +0200

    added global styling and some components

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,23 @@
+<template>
+  <button>
+    {{ msg }}
+  </button>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+// import 'Button.scss';
+
+@Options({
+  props: {
+    msg: String
+  }
+})
+export default class Button extends Vue {
+  msg!: string
+}
+</script>
+
+<style scoped lang="scss">
+  @import 'Button.scss';
+</style>
---
+++ src/components/Input/Input.vue
@@ -0,0 +1,20 @@
+<template>
+  <input type="text"> {{ msg }}
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+    msg: String
+  }
+})
+export default class Button extends Vue {
+  msg!: string
+}
+</script>
+
+<style scoped lang="scss">
+  @import 'Input.scss';
+</style>
