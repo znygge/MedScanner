59a292b4f1767ff4d69c428bb1c98231e91d6987
Johan Nygren <johan.nygren@knowit.se>
2021-10-15 19:18:27 +0200

    Created Camera module

---
+++ src/components/CameraComponent/CameraComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <video ref="video" autoplay muted playsinline />
+</template>
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {}
+})
+
+export default class CameraComponent extends Vue {
+  
+  public setVideoStream(video: MediaStream): void{
+    (this.$refs.video as HTMLVideoElement).srcObject = video;
+  }
+}
+</script>
+<style scoped lang="css" src="./CameraComponent.css" />
